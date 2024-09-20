407457718585a63772cc5591aa961e84bf20a89d
pareshbanerjee <48327853+pareshbanerjee@users.noreply.github.com>
2024-09-20 14:22:17 -0700

    Update Go.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           />
           <button
             type="submit"
-            className="  text-white p-2 rounded-lg bg-blue-500   w-1/2 text-3xl  "
+            className="  text-white p-2 rounded-lg bg-blue-500   w-1/2 text-xl  "
           >
             Go
           </button>
